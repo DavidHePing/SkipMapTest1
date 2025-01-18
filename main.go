c7309d2c5a4950b1203382428962a3f41b8c9da0
Dav He <s991179@gmail.com>
2025-01-18 16:41:40 +0800

    feat: test order

--- main.go
+++ main.go
@@ -4,5 +4,7 @@ func main() {
 	// SkipMap_test_set_read()
 	// SkipMap_test_delete1()
 	// SkipMap_test_delete2()
-	SkipMap_test_read_all()
+	// SkipMap_test_read_all1()
+	// SkipMap_test_order1()
+	SkipMap_test_order2()
 }
--- skipMap_test3.go
+++ skipMap_test3.go
@@ -6,7 +6,7 @@ import (
 	"github.com/zhangyunhao116/skipmap"
 )
 
-func SkipMap_test_read_all() {
+func SkipMap_test_read_all1() {
 	m0 := skipmap.NewString[int]()
 
 	for _, v := range []int{10, 12, 15, 17} {
@@ -15,6 +15,28 @@ func SkipMap_test_read_all() {
 
 	m0.Range(func(key string, value int) bool {
 		println("key:", key, "value:", value)
+		return true
+	})
+}
+
+func SkipMap_test_order1() {
+	m0 := skipmap.NewFunc[int, string](func(a, b int) bool { return a < b })
+	for _, v := range []int{21, 19, 17, 15} {
+		m0.Store(v, strconv.Itoa(v+200))
+	}
+	m0.Range(func(key int, value string) bool {
+		println("m0 found ", key, value)
+		return true
+	})
+}
+
+func SkipMap_test_order2() {
+	m0 := skipmap.NewFunc[int, string](func(a, b int) bool { return a < b })
+	for _, v := range []int{4, 1, 3, 2} {
+		m0.Store(v, strconv.Itoa(v+200))
+	}
+	m0.Range(func(key int, value string) bool {
+		println("m0 found ", key, value)
 		return true
 	})
 }
