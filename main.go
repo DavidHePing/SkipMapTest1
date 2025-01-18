3b78c3b46c46dc617723fe6ec8821fc3f741dc79
Dav He <s991179@gmail.com>
2025-01-18 16:31:33 +0800

    feat: test delete

--- main.go
+++ main.go
@@ -2,5 +2,6 @@ package main
 
 func main() {
 	// SkipMap_test1()
-	SkipMap_test_delete()
+	// SkipMap_test_delete1()
+	SkipMap_test_delete2()
 }
--- skipMap_test2.go
+++ skipMap_test2.go
@@ -7,7 +7,7 @@ import (
 	"github.com/zhangyunhao116/skipmap"
 )
 
-func SkipMap_test_delete() {
+func SkipMap_test_delete1() {
 	m0 := skipmap.NewString[int]()
 
 	for _, v := range []int{10, 12, 15} {
@@ -15,6 +15,19 @@ func SkipMap_test_delete() {
 	}
 
 	m0.Delete("10")
+	v, ok := m0.Load("10")
+	if ok {
+		fmt.Println("skipmap load 10 with value ", v)
+	} else {
+		fmt.Println("10 is not in skipmap")
+	}
+
+}
+
+func SkipMap_test_delete2() {
+	m0 := skipmap.NewString[int]()
+
+	m0.Delete("10") // not panic
 	v, ok := m0.Load("10")
 	if ok {
 		fmt.Println("skipmap load 10 with value ", v)
